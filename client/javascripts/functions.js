bc751adb482306da52e9781e327e2473d65ad5c5
Elise Ferguson <efergus3@uoregon.edu>
2020-05-08 18:30:02 -0600

    project 3

--- client/javascripts/functions.js
+++ client/javascripts/functions.js
@@ -9,42 +9,65 @@ let arrOf1To100 = fillArray(1, 100, 1);
 //log the array for inspection
 console.log(arrOf1To100);
 
-let bitBatBotOrNot = (n) => {
-   //your code goes here
-   let result = '';
-   if (n % 3 == 0) {
-     result = `${result}Bit`;
+const bitBatBotOrNot = (n) => {
+  let result = "";
+  if (n % 3 == 0) {
+    result = `${result}Bit`;
   }
-    if (n % 5 == 0) {
-      result = `${result}Bat`;
+  if (n % 5 == 0) {
+    result = `${result}Bat`;
   }
-    if (n % 7 == 0) {
-      result = `${result}Bot`;
+  if (n % 7 == 0) {
+    result = `${result}Bot`;
   }
-    else {
-      result = 'Not';
+  if ((n % 3 != 0) && (n % 5 != 0) && (n % 7 != 0)){
+    result = `Not`;
   }
   return result;
 };
 
-let findAllbitBatBotOrNots1 = (arr) => {
-   //your code goes here
-   //use map method
+let findAllbitBatBotOrNots1 = (arr) =>{
+  //use array.map and no loop
+  let resultArr = arr.map((elem) => {
+    return elem + ':' + bitBatBotOrNot(elem);
+  });
+  return resultArr;
 };
 
 let findAllbitBatBotOrNots2 = (arr) => {
-   //your code goes here
-   //use for statement
+  //use for loop and array.push
+  let result = [];
+  for (var i = 0; i < arr.length; i++) {
+    let currentNum = arr[i];
+    let elem = bitBatBotOrNot(currentNum);
+    let combinePhrase = currentNum + ':' + elem;
+    result.push(combinePhrase);
+  }
+  return result;
 };
 
 let findAllbitBatBotOrNots3 = (arr) => {
-   //your code goes here
-   //use for..of statement
+   //use for..of statement and array.push method
+   let arrtoString = arr.toString();
+   let currentNum = arrtoString.split(' ');
+   let result = [];
+   for (char of arr){
+     let elem = bitBatBotOrNot(char);
+     let combinePhrase = char + ':' + elem;
+     result.push(combinePhrase);
+   }
+   return result;
 };
 
 let findAllbitBatBotOrNots4 = (arr) => {
-   //your code goes here
-   //use forEach method
+   //use forEach method and array.push
+   let result = [];
+   arr.forEach((char) => {
+     let elem = bitBatBotOrNot(char);
+     let combinePhrase = char + ':' + elem;
+     result.push(combinePhrase);
+   });
+   return result;
 };
 
 //test the functions
